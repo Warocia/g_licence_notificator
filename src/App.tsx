08491361e291631376f05a437f9cae634399672a
Warocia <rauhala.antti@proton.me>
2023-01-28 07:59:44 +0200

    License values can now be edited

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 
 import {
-  BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
--- src/Functions/LicenceList.tsx
+++ src/Functions/LicenceList.tsx
@@ -5,18 +5,25 @@ import { License } from "../Classes/License";
 
 import LicenseUI from "./LicenseUI";
 
-export default function LicenceList({ licences }: { licences: Array<License> }) {
+interface Props {
+  licences: Array<License>;
+  handleRemoveLicence: (id: string) => void;
+  updateLicenseDate: (id: string, newDate : Date | null) => void;
+}
+
+export default function LicenceList({ licences, handleRemoveLicence, updateLicenseDate }: Props) {
     return (
       <Table striped bordered hover variant="dark">
         <thead>
           <tr>
-            <th>Lisenssi</th>
+            <th>Tunnus</th>
             <th>Asti voimassa</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {licences.map(license => {
-            return <LicenseUI key={license.LicenseNumber} license={license} />
+            return <LicenseUI key={license.id} license={license} handleRemoveLicence={handleRemoveLicence} updateLicenseDate={updateLicenseDate} />
           })}
          </tbody>
       </Table>
